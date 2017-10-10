17e2114a65ad1a983256a798262423632053f475
Marc Pittinsky <marclane@live.unc.edu>
2017-10-10 13:05:41 -0400

    roast sesh

--- bot.js
+++ bot.js
@@ -16,30 +16,35 @@ function respond() {
 
   this.res.writeHead(200);
 
-  if(request.text && sOTD.test(request.text)) {
-    postMessage("Text: " + JSON.stringify(request) + "\nName: " + request.name);
+  if((request.name.indexOf("Marc") !== -1 || request.name.indexOf("Jake") !== -1) && request.text.indexOf("Scumguy roast") !==-1) {
+      roastedPerson = request.text.substring(13,request.text.length);
+      postMessage("You suck "+ roastedPerson);
   }
-  if(beer.test(request.text)) {
-    postMessage("You guys should be drinking " + beers[Math.floor(Math.random()*beers.length)] + " tonight");
-  }
-  if(dinner.test(request.text)) {
-    postMessage("Let's get " + restaurants[Math.floor(Math.random()*restaurants.length)] + " tonight");
-  }
-  if(request.text.indexOf("What did Andrew buy?") !== -1) {
-    postMessage("The Blue Moons");
-  }
-  if(smallFry.test(request.text)) {
-    postMessage("When you get screwed out of a small fry", "https://i.groupme.com/540x960.jpeg.829fb06a5d94408895a6e7d59562a1ab");
-  }
-  if(request.text.indexOf("Push me to the edge") !== -1) {
-    postMessage("","https://i.groupme.com/750x1334.jpeg.4e0db5f28b65414fb43e322cd9146a91")
-  }
-  if(request.name.indexOf("Andre") !== -1 ) {
-    postMessage("I'm pretty spiritually aware and religious. It adds a layer to my life that does assist me greatly.");
-  }
-  if(request.text.indexOf("$") !== -1) {
-    postMessage("Money is the root of all evil " + request.name + "!");
-  }
+
+  // if(request.text && sOTD.test(request.text)) {
+  //   postMessage("Text: " + JSON.stringify(request) + "\nName: " + request.name);
+  // }
+  // if(beer.test(request.text)) {
+  //   postMessage("You guys should be drinking " + beers[Math.floor(Math.random()*beers.length)] + " tonight");
+  // }
+  // if(dinner.test(request.text)) {
+  //   postMessage("Let's get " + restaurants[Math.floor(Math.random()*restaurants.length)] + " tonight");
+  // }
+  // if(request.text.indexOf("What did Andrew buy?") !== -1) {
+  //   postMessage("The Blue Moons");
+  // }
+  // if(smallFry.test(request.text)) {
+  //   postMessage("When you get screwed out of a small fry", "https://i.groupme.com/540x960.jpeg.829fb06a5d94408895a6e7d59562a1ab");
+  // }
+  // if(request.text.indexOf("Push me to the edge") !== -1) {
+  //   postMessage("","https://i.groupme.com/750x1334.jpeg.4e0db5f28b65414fb43e322cd9146a91")
+  // }
+  // if(request.name.indexOf("Andre") !== -1 ) {
+  //   postMessage("I'm pretty spiritually aware and religious. It adds a layer to my life that does assist me greatly.");
+  // }
+  // if(request.text.indexOf("$") !== -1) {
+  //   postMessage("Money is the root of all evil " + request.name + "!");
+  // }
   
   // if(bored.test(request.text)) {
   //   postMessage("Copy and Paste the message below and send it with your first move to start");
