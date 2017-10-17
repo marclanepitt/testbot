c530f15ffb018f7eb13a21e420e8cdf69563f790
Marc Pittinsky <marclane@live.unc.edu>
2017-10-17 13:19:16 -0400

    added compliments

--- bot.js
+++ bot.js
@@ -25,22 +25,13 @@ function respond() {
   this.res.writeHead(200);
 
   if(request.text.indexOf("Scumguy roast") !==-1) {
-      var roastedPerson = "";
-      if(request.text.substring(13,request.text.length) == "me") {
-        roastedPerson = request.name;
-      } else {
-        roastedPerson = request.text.substring(13,request.text.length);
-      }
+      var roastedPerson = request.text.substring(13,request.text.length);
       var insult = array[index];
       insult = insult.substring(4,insult.length-1).toLowerCase();
       postMessage(roastedPerson+"," + insult);
-  } else if(request.text.indexOf("Scumguy compliment") !==-1) {
-      var complimentedPerson = "";
-      if(request.text.substring(18,request.text.length) == "me") {
-        complimentedPerson = request.name;
-      } else {
-        complimentedPerson = request.text.substring(18,request.text.length);
-      }
+  }
+  if(request.text.indexOf("Scumguy compliment") !==-1) {
+      var complimentedPerson = request.text.substring(18,request.text.length);
       var compliment = compliments[index];
       compliment = compliment.toLowerCase();
       postMessage(complimentedPerson+"," + compliment);
