6a93987dbf15de06c36189dadc2224e5631891e7
Marc Pittinsky <mpittinsky@Marcs-MacBook-Air.local>
2018-07-23 16:55:07 -0400

    m

--- bot.js
+++ bot.js
@@ -127,7 +127,7 @@ function sendLove(names) {
     var cm = function() {
       body = JSON.parse(body);
       var response = body.percentage+"% compatibility"
-      postMessage(response + " " + body.result);
+      postMessage(""+response + ": " + body.result);
     }
     resp.on('data', cb);
     resp.on('end', cm);
